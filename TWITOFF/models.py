1650076164486f05a1d0c784f370b99e3459710e
davidanagy <davidanagy@users.noreply.github.com>
2019-11-12 11:02:34 -0800

    following along on lecture

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -8,10 +8,21 @@ DB = SQLAlchemy()
 
 class User(DB.Model):
     """Twitter useres that we analyze"""
-    id = DB.Column(DB.Integer, primary_key=True)
+    id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
+
+    def __repr__(self):
+        return f'<User {self.name}>'
+
 
 class Tweet(DB.Model):
     """The user's tweets from Twitter"""
-    id = DB.Column(DB.Integer, primary_key=True)
-    text = DB.Column(DB.Unicode(200))
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(300))
+    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+    embedding = DB.Column(DB.PickleType, nullable=False)
+
+    def __repr__(self):
+        return f'<Tweet {self.text}>'
